3d32225cf518c31eea7564c925e217f0c1cde48f
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2024-03-25 21:59:11 -0300

    a

--- BANCO DE DADOS/banco_av/att01.sql
+++
@@ -1,8 +0,0 @@
-SELECT u.cod_usuario, v.cod_produto
-FROM usuarios u
-JOIN vendas v ON u.cod_usuario = v.cod_produto;
-
-select usuarios.cod_usuario , usuarios.faixa_etaria 
-from  usuarios
-where usuarios.faixa_etaria = 'Entre 51 a 60 anos'
-;
